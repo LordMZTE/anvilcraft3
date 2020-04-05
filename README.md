a74750436b93150dc38c596e1183e1de8402ca63
LordMZTE <lord@mzte.de>
2020-04-05 16:32:12 +0200

    Added script for downloading mods

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # anvilcraft3
 Execute gradlew buildTwitch to create the Twitch zip file in build/lib
+or run python3 downloadMods.py to download all mods into a directory
 
 tweaked fork by lordmzte
---
+++ downloadMods.py
@@ -0,0 +1,38 @@
+import json
+from urllib import request, parse
+import ntpath
+import os
+
+#Constants:
+manifestlocation="src/twitch/manifest.json"
+if os.path.exists("mods"):
+    if os.listdir("mods"):
+        print("mods directory is not empty, delete or empty")
+        quit()
+
+try:
+    with open(manifestlocation, "r") as manifestfile:
+        manifestdata = manifestfile.read()
+except:
+    print("manifest not found")
+    quit()
+manifestobj = json.loads(manifestdata)
+
+downloadLinks = []
+filecount = len(manifestobj["files"])
+
+i = 0
+for file in manifestobj["files"]:
+    i += 1
+    with request.urlopen("https://addons-ecs.forgesvc.net/api/v2/addon/" + str(file["projectID"]) + "/file/" + str(file["fileID"]) + "/download-url", timeout=1000) as response:
+        responseLink = response.read().decode("utf-8")
+        downloadLinks.append(responseLink)
+        print("(" + str(i) + "/" + str(filecount) + ") Got Download Link For " + ntpath.basename(responseLink))
+
+i = 0
+filecount = len(downloadLinks)
+for link in downloadLinks:
+    i += 1
+    filename = ntpath.basename(link)
+    request.urlretrieve(link, "mods/" + filename)
+    print("(" + str(i) + "/" + str(filecount) + ") Downloaded " + filename)
